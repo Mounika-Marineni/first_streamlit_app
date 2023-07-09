4aab7e1ffe9039d6c92ac1384de55c645654cbcf
Mounika-Marineni <138757595+Mounika-Marineni@users.noreply.github.com>
2023-07-09 21:46:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
+#import streamlit
+#import pandas
+#import requests
+#import snowflake.connector
 from urllib.error import URLError
 streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast Favourites')
@@ -39,7 +39,7 @@ def get_fruit_load_list():
             with my_cnx.cursor() as my_cur:
                   my_cur.execute("select * from fruit_load_list")
                   return my_cur.fetchall()
-if streamlit.button('Get Fruit Load List"):      
+if streamlit.button('Get Fruit Load List'):      
             my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
             my_data_rows = get_fruit_load_list()
             streamlit.dataframe(my_data_rows)
